c7aeaa626225c85288f9761241e06f5947ea15d5
AE <57876833+AE-C0de@users.noreply.github.com>
2023-03-29 16:22:02 -0600

    add size change

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -8,25 +8,48 @@ sketchContainer.addEventListener('mouseleave', () => {
     isMouseDown = false;
 })
 
-for(i = 0; i < gridXSize; i++){
-    let rowDiv = document.createElement('div');
-    rowDiv.style.cssText = 'display: flex; flex: 1';
-    sketchContainer.appendChild(rowDiv);
-    for(j = 0; j < gridXSize; j++){
-        let box = document.createElement('div');
-        box.draggable = false;
-        box.addEventListener("mousedown", () => {
-            isMouseDown = true;
-        })
-        box.addEventListener("mouseup", () => {
-            isMouseDown = false;
-        })
-        box.addEventListener("mouseenter",  () => {
-            if(isMouseDown == true){
-                box.style.backgroundColor = paintColor;
-            }
-        })
-        box.classList.add('sketchBox');
-        rowDiv.appendChild(box);
+document.getElementById('sizeChange').addEventListener("click", () => {
+    let size = window.prompt("Input grid size");
+
+    if(size > 100){
+        size = 100;
     }
+
+    gridXSize = size;
+    gridYSize = size;
+    clearGrid();
+    populateGrid();
+})
+
+function populateGrid(){
+    for(i = 0; i < gridXSize; i++){
+        let rowDiv = document.createElement('div');
+        rowDiv.style.cssText = 'display: flex; flex: 1';
+        sketchContainer.appendChild(rowDiv);
+        for(j = 0; j < gridXSize; j++){
+            let box = document.createElement('div');
+            box.draggable = false;
+            box.addEventListener("mousedown", () => {
+                isMouseDown = true;
+            })
+            box.addEventListener("mouseup", () => {
+                isMouseDown = false;
+            })
+            box.addEventListener("mouseenter",  () => {
+                if(isMouseDown == true){
+                    box.style.backgroundColor = paintColor;
+                }
+            })
+            box.classList.add('sketchBox');
+            rowDiv.appendChild(box);
+        }
+    }
+}
+
+function clearGrid(){
+    sketchContainer.innerHTML = ' ';
+}
+
+window.onload = () => {
+    populateGrid();
 }
